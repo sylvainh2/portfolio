231d79c563a5c3065baea9b63ebeb755e2f84e2a
Sylvain Crouzier <sylvain.crouzier@free.fr>
2026-05-12 01:48:58 +0200

    envoi de mail github pages

--- src/pages/Mail.jsx
+++ src/pages/Mail.jsx
@@ -1,30 +1,39 @@
 import Header from '../components/Header';
 import ReturnB from '../components/ReturnB';
+import emailjs from "@emailjs/browser";
+import { useRef } from 'react';
 
 function Mail(){
-    const handleSubmitJoin = async (event)=>{
+    const form = useRef();
+    const handleSubmitJoin = (event)=>{
         event.preventDefault();
+        const serviceID = 'default_service';
+        const templateID = 'template_9qcgitk';
+        const publicKEY = 'I8Ndgl3Khqs3TUvCL';
         let email= event.target.email.value;
         let message = event.target.message.value;
         let sujet = event.target.sujet.value;
         if (email && message && sujet){
             //traitement envoi email//
-            const emailSender = await fetch("http://localhost:5000/send-email",{
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
+            emailjs
+            .sendForm(
+                serviceID,
+                templateID,
+                form.current,
+                publicKEY
+            )
+            .then(
+                () => {
+                document.querySelector(".sendWindow").setAttribute("class","sendWindow absCont");
+                setTimeout(()=>{
+                    document.querySelector(".sendWindow").setAttribute("class","sendWindow absCont wHidden");
+                },3000);
                 },
-                body: JSON.stringify({
-                    email,
-                    sujet,
-                    message
-                })
-            })
+                (error) => {
+                console.log(error.text);
+                }
+            );
             document.querySelector(".mailForm").reset();
-            document.querySelector(".sendWindow").setAttribute("class","sendWindow absCont");
-            setTimeout(()=>{
-                document.querySelector(".sendWindow").setAttribute("class","sendWindow absCont wHidden");
-            },3000);
         }
     };
 
@@ -33,14 +42,14 @@ function Mail(){
         {/* <Header /> */}
         <main className="mail">
             <div className="sendWindow absCont wHidden">Message Envoyé</div>
-            <form className="mailForm">
+            <form ref={form} className="mailForm" onSubmit={handleSubmitJoin}>
                 <label className="inputMail">email:</label>
                 <input className="inputMail mailEffect email" type="email" name="email" placeholder="Entrez votre Email svp"/>
                 <label className="inputMail">sujet</label>
                 <input className="inputMail mailEffect sujet" type="text" name="sujet"/>
                 <label className="inputMail">message:</label>
                 <textarea className="inputMail mailEffect areaEffect message" type="text" cols="50" rows="10" name="message" />
-                <button className="mailBtn inputMail">Non supporté sur github pages</button>
+                <button className="mailBtn inputMail">Envoyer</button>
             </form>
         </main>
         <ReturnB />
